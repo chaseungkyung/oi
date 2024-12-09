9fd04947ba865f54232b29e856d1ca4ef5907bd4
user2 <user2@SISTC77>
2024-12-09 11:30:50 +0900

    1208 HEADER 수정 CHA

--- oi/src/main/java/com/oi/controller/RecordController.java
+++ oi/src/main/java/com/oi/controller/RecordController.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @Controller
 public class RecordController {
 
-	@RequestMapping(value = "/completerecordmeal/main")
+	@RequestMapping(value = "/recordmeal/main")
 	public ModelAndView intomain(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		return new ModelAndView("recordmeal/recordmain");
